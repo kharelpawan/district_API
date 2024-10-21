0757c7aebd1bb8befbac1f991bae4de052ed82e5
Kharel Pawan <kharelpawan.np@gmail.com>
2024-10-21 17:17:00 +0545

    port changed

--- index.js
+++ index.js
@@ -2,11 +2,11 @@ const express = require('express');
 const app = express();
 const districtsRouter = require('./routes/districts'); // Import the router
 
-const port = 3000;
+//const port = 3000;
 
 // Use the districts router
 app.use('/api/districts', districtsRouter);
 
 app.listen(port, () => {
-  console.log(`Server is running on http://localhost:${port}`);
+  //console.log(`Server is running on http://localhost:${port}`);
 });
